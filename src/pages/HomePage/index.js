27fbf94a6aa8ebc92ee7957f6355d521d68fa643
Lhester Monroyo <lhestermoon3011@gmail.com>
2021-05-14 13:31:24 +0800

    home page ui, added background image on main component

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Main from '../../components/Main';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const HomePage = () => {
+  const { user } = useAuth0();
+
   return (
     <Main>
-      <h1>Home Page</h1>
+      <div className='home-container'>
+        <h2>{user?.name}</h2>
+        <p>https://github.com/{user?.nickname}</p>
+        <form className='search-container'>
+          <div className='input-prepend-container'>
+            <i className='fa fa-search fa-fw input-prepend' />
+            <input type='text' name='search' placeholder='City' />
+          </div>
+          <button className='btn-default search-btn'>Display Weather</button>
+        </form>
+      </div>
     </Main>
   );
 };
--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -5,8 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import LoadingScreen from '../../components/LoadingScreen';
 
 const LandingPage = () => {
-  const { loginWithRedirect, logout, isLoading, isAuthenticated, user } =
-    useAuth0();
+  const { loginWithRedirect, isLoading, isAuthenticated } = useAuth0();
 
   if (isLoading) {
     return <LoadingScreen loadingMsg='Loading, please wait...' />;
@@ -19,6 +18,7 @@ const LandingPage = () => {
   return (
     <Main>
       <div className='landing-container'>
+        <h1>Hello!</h1>
         <p>
           Welcome to the weather forecast web application. Please login with
           your Github user to use the application and view the weather in your
