e0fc78544036399e61bae56a25c0efe7574065be
Lhester Monroyo <lhestermoon3011@gmail.com>
2021-05-14 12:24:31 +0800

    login and logout functionalities

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Routes from './routes';
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+console.log(domain);
+
 const App = () => {
   return (
-    <React.Fragment>
-      <Navbar />
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </React.Fragment>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <React.Fragment>
+        <Navbar />
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </React.Fragment>
+    </Auth0Provider>
   );
 };
 
--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Main from '../../components/Main';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const LandingPage = () => {
+  const { loginWithRedirect, logout, isLoading, isAuthenticated, user } =
+    useAuth0();
+
+  if (isLoading) {
+    return (
+      <div className='landing-container'>
+        <p>Loading, please wait...</p>
+      </div>
+    );
+  }
+
+  if (isAuthenticated) {
+    console.log('Logged in', user);
+  }
+
   return (
     <Main>
       <div className='landing-container'>
@@ -10,7 +26,12 @@ const LandingPage = () => {
           your Github user to use the application and view the weather in your
           city.
         </p>
-        <button className='btn-default'>Login</button>
+        <button className='btn-default' onClick={() => loginWithRedirect()}>
+          Login
+        </button>
+        <button className='btn-default' onClick={() => logout()}>
+          Logout
+        </button>
       </div>
     </Main>
   );
